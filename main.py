7b09f60e33e15fc9e7ad8fba2156ce35ff622b2b
JonTrav55 <johan.mats.96@gmail.com>
2025-03-22 19:49:42 +0100

    finsihed the bookbot with books, frankenstein, mobydick, prideandprejudice

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_word_count
 from stats import get_all_characters
 from stats import sort_char
@@ -9,35 +11,37 @@ def get_book_text(filepath):
     
    
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    #print(book_text)
-    word_count = get_word_count(book_text)
-    #print(f"{word_count} words found in the document")
     
+    if len(sys.argv) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
+    word_count = get_word_count(book_text)
     amount = get_all_characters(book_text)
     sort_char(amount)
-    
-    path = "books/frankenstein.txt"
+        
+    path = sys.argv[1]
     book_text = get_book_text(path)
     word_count = get_word_count(book_text)
-    
+        
     amount = get_all_characters(book_text)
     sorted_chars = sort_char(amount)
-    
-    # Print the report
+        
+        # Print the report
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    
-    # Print only alphabetical characters
+        
+        # Print only alphabetical characters
     for char_dict in sorted_chars:
         char = char_dict["char"]
         count = char_dict["count"]
         if char.isalpha():
             print(f"{char}: {count}")
-    
+        
     print("============= END ===============")
 
 main()
